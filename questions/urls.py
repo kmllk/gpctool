99d0f2111d6df460d69616dfcc7794bb36e5d767
kmllk <kamillakurmanova@gmail.com>
2025-04-19 20:53:19 +0100

    urls are updated

--- questions/urls.py
+++ questions/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     #url(r'^$', views.import_sheet, name="import_sheet"),
     path('', views.homepage, name='homepage'),
-    re_path(r'(?P<question_id>[0-9]+)/$', views.question, name='question'),
-    path('error.html/', views.error, name='error'),
-    path('index.html/', views.homepage, name='homepage'),
-    re_path(r'(?P<question_id>[0-9]+)/(?P<answer>yes|no)/result.html$', views.result, name='result'),
-    path('promo.html/', views.promo, name='promo'),
+    re_path('questions/'+r'(?P<question_id>[0-9]+)/$', views.question, name='question'),
+    path('questions/error.html', views.error, name='error'),
+    path('questions/index.html', views.homepage, name='homepage'),
+    re_path('questions/'+r'(?P<question_id>[0-9]+)/(?P<answer>yes|no)/result.html$', views.result, name='result'),
+    path('questions/promo.html', views.promo, name='promo'),
 ]
