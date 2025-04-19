0a4fa38387a8eb94ac2d27e675378172567e21fc
kmllk <kamillakurmanova@gmail.com>
2025-04-19 19:41:52 +0100

    Changed html pages.

--- questions/urls.py
+++ questions/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import re_path, path
 from . import views
 
 urlpatterns = [
+    #url(r'^$', views.import_sheet, name="import_sheet"),
     path('', views.homepage, name='homepage'),
+    re_path(r'(?P<question_id>[0-9]+)/$', views.question, name='question'),
+    path('error.html/', views.error, name='error'),
+    path('index.html/', views.homepage, name='homepage'),
+    re_path(r'(?P<question_id>[0-9]+)/(?P<answer>yes|no)/result.html$', views.result, name='result'),
+    path('promo.html/', views.promo, name='promo'),
 ]
